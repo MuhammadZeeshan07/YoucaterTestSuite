e62061f60a616dcb6a7e4dc5681843686f2ddeb1
MuhammadZeeshan07 <zeeshanfiazcs@gmail.com>
2025-08-27 21:05:56 +0500

    Fix: add path module import for file upload

--- pages/VendorRequestPage.js
+++ pages/VendorRequestPage.js
@@ -16,13 +16,21 @@ class VendorRequestPage extends BasePage {
     await this.waitForVisible(this.findPartners);
     await this.page.locator(this.findPartners).scrollIntoViewIfNeeded();
     await this.click(this.findPartners);
+    await this.waitForVisible(this.searchVendorInput);
+    await this.page.locator(this.searchVendorInput).scrollIntoViewIfNeeded();
     await this.fill(this.searchVendorInput, 'ambala');
+    await this.waitForVisible(this.vendorLink);
+    await this.page.locator(this.vendorLink).scrollIntoViewIfNeeded();
     await this.click(this.vendorLink);
+    await this.waitForVisible(this.sendRequestStrong);
+    await this.page.locator(this.sendRequestStrong).scrollIntoViewIfNeeded();
     await this.click(this.sendRequestStrong);
     await page.locator('(//div[contains(@class,"sharePartnerEventsList")]//a[contains(@class,"simpleEventCard")])[1]').click();
     await this.waitForVisible(this.sendRequestBtn);
     await page.locator(this.sendRequestBtn).scrollIntoViewIfNeeded();
     await this.click(this.sendRequestBtn);
+    await this.waitForVisible(this.myDashboard);
+    await this.page.locator(this.myDashboard).scrollIntoViewIfNeeded();
     await this.click(this.myDashboard);
   }
 }
